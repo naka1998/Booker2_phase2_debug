7fa6f1e7c4efed038316e886cfcccb9900ea8023
naka1998 <bjtn1290411@gn.iwasaki.ac.jp>
2020-03-21 19:25:00 +0900

    作成 検索機能② 前方一致など

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,13 +2,14 @@ class SearchController < ApplicationController
   def search
     target = params[:target].to_i
     body = params[:body]
+    method = params[:matching_method].to_i
     @book = Book.new
     case target
       when 1
-        @users = User.where(name: body)
+        @users = User.search(body, method)
         render "users/index"
       when 2
-        @books = Book.where(title: body)
+        @books = Book.search(body, method)
         render "books/index"
       else
         redirect_back(fallback_location: root_path)
